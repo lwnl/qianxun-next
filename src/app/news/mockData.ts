e1b9654754e3e2bddf88b7ac7b269c109a64fb49
lwnl <liangwang.fr@gmail.com>
2025-06-13 12:42:06 +0200

    要闻页面，页面切换

---
+++ src/app/news/mockData.ts
@@ -0,0 +1,27 @@
+export const newsList = Array.from({ length: 20 }, (_, index) => {
+  const date = new Date();
+  date.setDate(date.getDate() - index); // 今天开始，往前推一天、两天...
+  return {
+    id: index + 1,
+    title: `第${index + 1}条新闻`,
+    content: `这是第${index + 1}条新闻的简短内容， ${randomText(100)}。`,
+    editor: `编辑${index + 1}`,
+    createdAt: date.toISOString(),
+  };
+}) as {
+  id: number;
+  title: string;
+  content: string;
+  editor: string;
+  createdAt: string;
+}[];
+
+function randomText (length: number): string {
+  let text = ''
+  for (let i = 0; i < length; i++) {
+    const charCode = Math.floor(Math.random() * (0x9FA5 - 0x4E00)) + 0x4E00
+    text += String.fromCharCode(charCode)
+  }
+  return text
+}
+
--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,8 +1,58 @@
+"use client";
+
+import { newsList } from "./mockData";
+import { useState } from "react";
+
+const pageSize: number = 5;
+const totalPages: number = Math.floor(newsList.length / pageSize);
+
 export default function NewsPage() {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const currentNewsList = newsList.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize
+  );
+
   return (
-    <div>
-      <h1>中国要闻</h1>
-      <p>这里是中国要闻页面的内容。</p>
-    </div>
+    <>
+      <h4>最新要闻</h4>
+      <div className="important-news">
+        {currentNewsList.map((news) => (
+          <div key={news.id}>
+            <h3>{news.title}</h3>
+            <p>{news.createdAt}</p>
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <span
+          onClick={() => {
+            setCurrentPage(Math.max(currentPage - 1, 1));
+          }}
+        >
+          上一页
+        </span>
+
+        {Array.from({ length: totalPages }).map((_, index) => (
+          <span key={index} onClick={() => setCurrentPage(index + 1)}>
+            {index + 1}
+          </span>
+        ))}
+
+        <span
+          onClick={() => {
+            setCurrentPage(Math.min(currentPage + 1, totalPages));
+          }}
+        >
+          下一页
+        </span>
+      </div>
+
+      <div className="ranking-list">
+        <h4>排行榜</h4>
+      </div>
+    </>
   );
 }
